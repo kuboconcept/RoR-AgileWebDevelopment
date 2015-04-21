ba8db6730a63a37edcbb3bdcf7e9ca43ea3367d4
AriGunawan <ari.gunawan@mitrais.com>
2015-04-21 16:28:23 +0800

    Add Store and Unit Test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,17 +9,29 @@ class ProductsControllerTest < ActionController::TestCase
       image_url: 'lorem.jpg',
       price: 19.95
     }
+    @total_product = 3
   end
 
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+
+    assert_select "#main .list_image", @total_product
+    assert_select "#main .list_description", @total_product
+    assert_select "#main .list_action", @total_product
+    assert_select "#main .list_action a", @total_product * 3
   end
 
   test "should get new" do
     get :new
     assert_response :success
+
+    assert_select "#main input#product_title", 1
+    assert_select "#main textarea#product_description", 1
+    assert_select "#main input#product_image_url", 1
+    assert_select "#main input#product_price", 1
+    assert_select "#main input[type='submit']", 1
   end
 
   test "should create product" do
@@ -33,11 +45,19 @@ class ProductsControllerTest < ActionController::TestCase
   test "should show product" do
     get :show, id: @product
     assert_response :success
+
+    assert_select "#main p", 4
   end
 
   test "should get edit" do
     get :edit, id: @product
     assert_response :success
+
+    assert_select "#main input#product_title", 1
+    assert_select "#main textarea#product_description", 1
+    assert_select "#main input#product_image_url", 1
+    assert_select "#main input#product_price", 1
+    assert_select "#main input[type='submit']", 1
   end
 
   test "should update product" do
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
-  # end
+  # end  
 
   test "product attributes must not be empty" do
   	product = Product.new
