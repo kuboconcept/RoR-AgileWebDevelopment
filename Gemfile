541162119f1af21ac72a6db6e109d3709e230891
Kuboconcept <Kuboconcept@gmail.com>
2015-04-27 15:32:25 +0800

    Adding User

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use unicorn as the app server
 # gem 'unicorn'
@@ -38,3 +38,6 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
+
+# Use ActiveModel has_secure_password
+#gem 'bcrypt-ruby', '~> 3.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424035332) do
+ActiveRecord::Schema.define(version: 20150427061419) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20150424035332) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
